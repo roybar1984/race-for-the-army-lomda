7e0fb996dabc9ee87a40af34015495524ad43775
palmach <lomdatpalmach@gmail.com>
2022-02-15 19:33:28 +0200

    added the first raw of input letters

--- src/containers/gamePage/GamePage.js
+++ src/containers/gamePage/GamePage.js
@@ -12,7 +12,21 @@ function GamePage(props) {
         </p>
         <div className="letter-container">
           <div className="letters-row top-row-letters">
-            <div></div>
+            <input
+              className="letter-input letter0"
+              // value={value}
+              // onChange={handelChange}
+            />
+            <input
+              className="letter-input letter1"
+              // value={value}
+              // onChange={handelChange}
+            />
+            <input
+              className="letter-input letter2"
+              // value={value}
+              // onChange={handelChange}
+            />
           </div>
         </div>
       </div>

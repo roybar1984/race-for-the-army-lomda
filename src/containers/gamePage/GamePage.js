93c45fee455998f539a81d2ae81ad85d050c8e38
palmach <lomdatpalmach@gmail.com>
2022-02-17 13:43:27 +0200

    added input fields for the game page and added instructions for the game

--- src/containers/gamePage/GamePage.js
+++ src/containers/gamePage/GamePage.js
@@ -5,29 +5,64 @@ import { Markup } from "interweave";
 function GamePage(props) {
   return (
     <div className="game-page-container">
-      <h1 className="game-title"> {props.title}</h1>
+      <h1 className="game-title">
+        {" "}
+        <Markup content={props.title} />
+      </h1>
       <div className="text-container">
         <p className="text-paragrapgh">
           <Markup content={props.text} />
         </p>
-        <div className="letter-container">
-          <div className="letters-row top-row-letters">
-            <input
-              className="letter-input letter0"
-              // value={value}
-              // onChange={handelChange}
-            />
-            <input
-              className="letter-input letter1"
-              // value={value}
-              // onChange={handelChange}
-            />
-            <input
-              className="letter-input letter2"
-              // value={value}
-              // onChange={handelChange}
-            />
-          </div>
+      </div>
+      <div className="letter-container">
+        <div className="letters-row top-row-letters">
+          <input
+            className="letter-input letter0"
+            // value={value}
+            // onChange={handelChange}
+          />
+          <input
+            className="letter-input letter1"
+            // value={value}
+            // onChange={handelChange}
+          />
+          <input
+            className="letter-input letter2"
+            // value={value}
+            // onChange={handelChange}
+          />
+        </div>
+        <div className="letters-row low-row-letters">
+          <input
+            className="letter-input letter3"
+            // value={value}
+            // onChange={handelChange}
+          />
+          <input
+            className="letter-input letter4"
+            // value={value}
+            // onChange={handelChange}
+          />
+          <input
+            className="letter-input letter5"
+            // value={value}
+            // onChange={handelChange}
+          />
+          <input
+            className="letter-input letter6"
+            // value={value}
+            // onChange={handelChange}
+          />
+          <input
+            className="letter-input letter7"
+            // value={value}
+            // onChange={handelChange}
+          />
+          <input
+            className="letter-input letter8"
+            // value={value}
+            // onChange={handelChange}
+          />
         </div>
       </div>
     </div>
